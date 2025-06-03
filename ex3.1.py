12bbf6e30a834102438f5435220ee0662cd2a527
Haridiii <abdulrahmanmustafa553@gmail.com>
2025-06-03 13:45:26 +0300

    Conditional Execution

---
+++ ex3.1.py
@@ -0,0 +1,11 @@
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+fh = float (hrs)
+fr = float (rate)
+if fh > 40 :
+    so0 = fr * fh
+    so = (fh - 40.0) * (fr * 0.5) 
+    xp = so0 + so
+else:
+    xp = fh * fr     
+print (xp) 
---
+++ ex3.2.py
@@ -0,0 +1,15 @@
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+try :
+     fh = float (hrs)
+     fr = float (rate)
+except :
+    print ("Error, please enter numeric input")
+    quit ()     
+if fh > 40 :
+    so0 = fh * fr
+    so = (fh - 40.0) * (fr * 0.5) 
+    xp = so0 + so
+else:
+    xp = fh * fr     
+print (xp) 
